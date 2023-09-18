e30eb09317cba63172e9d6acf2815b85e7146fa7
maimemory <87125676+maimemory@users.noreply.github.com>
2023-09-18 21:34:24 +0700

    add process.env.PUBLIC_URL for GitHub Pages

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -17,7 +17,7 @@ function Menu() {
         {/* Brand Logo */}
         <a href="index3.html" className="brand-link">
           <img
-            src="/dist/img/AdminLTELogo.png"
+            src={process.env.PUBLIC_URL + "/dist/img/AdminLTELogo.png"}
             alt="AdminLTE Logo"
             className="brand-image img-circle elevation-3"
             style={{ opacity: ".8" }}
@@ -30,7 +30,7 @@ function Menu() {
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="image">
               <img
-                src="/dist/img/user2-160x160.jpg"
+                src={process.env.PUBLIC_URL + "/dist/img/user2-160x160.jpg"}
                 className="img-circle elevation-2"
                 alt="User Image"
               />
